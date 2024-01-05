8f7836074b6ebace6428266b3737a4131df0ff2e
Arbaz Malek <malekarbaz67@gmail.com>
2024-01-05 17:37:09 +0530

    otpless sdk setup completed

--- app/src/main/java/com/androidnative/app/MainActivity.java
+++ app/src/main/java/com/androidnative/app/MainActivity.java
@@ -1,14 +1,65 @@
-package com.androidnative.app;
+﻿package com.androidnative.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.otpless.dto.OtplessResponse;
+import com.otpless.main.OtplessManager;
+import com.otpless.main.OtplessView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    OtplessView otplessView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialise OtplessView
+        otplessView = OtplessManager.getInstance().getOtplessView(this);
+        final JSONObject extras = new JSONObject();
+        try {
+            extras.put("method", "get");
+            final JSONObject params = new JSONObject();
+            params.put("cid", "I9HXYP33C1K9Z61ZIF0MI1PY4VZOFX6Q");
+            extras.put("params", params);
+            extras.put("crossButtonHidden", true);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        otplessView.setCallback(this::onOtplessCallback, extras);
+        otplessView.showOtplessLoginPage(extras, this::onOtplessCallback);
+        otplessView.verifyIntent(getIntent());
+    }
+
+    private void onOtplessCallback(OtplessResponse response) {
+        if (response.getErrorMessage() != null) {
+// todo error handing
+        } else {
+            final String token = response.getData().optString("token");
+// todo token verification with api
+            Log.d("Otpless", "token: " + token);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (otplessView != null) {
+            otplessView.verifyIntent(intent);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        // make sure you call this code before super.onBackPressed();
+        if (otplessView.onBackPressed()) return;
+        super.onBackPressed();
     }
 }
