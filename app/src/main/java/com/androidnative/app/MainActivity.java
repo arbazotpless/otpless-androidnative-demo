2762d1e116d74498e859e5e8625762b5e3dab802
Arbaz Malek <malekarbaz67@gmail.com>
2024-01-19 15:27:46 +0530

    update: otpless integrated, uxmode anf demo added

--- app/src/main/java/com/androidnative/app/MainActivity.java
+++ app/src/main/java/com/androidnative/app/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.otpless.dto.OtplessResponse;
 import com.otpless.main.OtplessManager;
 import com.otpless.main.OtplessView;
+import com.otpless.utils.Utility;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        Button button1 = findViewById(R.id.button);
+
+        //******************************************************** */
+        //This function will tell if WhatsApp is Installed or not.
+        // If you are using only whatsapp authentication then you can hide visibility of Login button using this function,
+        // if user doesn't have whatsapp installed on device.
+        //******************************************************** */
+
+        if (Utility.isWhatsAppInstalled(this)){
+            Toast.makeText(this, "whatsapp is installed in device", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Whatsapp is not installed in device", Toast.LENGTH_SHORT).show();
+        }
+
 
         // Initialise OtplessView
         otplessView = OtplessManager.getInstance().getOtplessView(this);
