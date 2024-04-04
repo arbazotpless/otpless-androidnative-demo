b8a1af097bf173fc75c08cccf510a7e9451eed6a
Arbaz Malek <arbaz@otpless.com>
2024-04-04 11:06:43 +0530

    otpless sdk setup completed

--- app/src/main/java/com/androidnative/app/MainActivity.java
+++ app/src/main/java/com/androidnative/app/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Initialise OtplessView
         otplessView = OtplessManager.getInstance().getOtplessView(this);
         OtplessRequest request = new OtplessRequest("RI7MXA25OHAGDHBXMFB1") //replace with your appid provided in documentation
-                .addExtras("crossButtonHidden","true");  //use this parameter to hide cross button
+                                    .addExtras("crossButtonHidden","true");  //use this parameter to hide cross button
         otplessView.setCallback(request, this::onOtplessCallback);
         otplessView.showOtplessLoginPage(request, this::onOtplessCallback);
         otplessView.verifyIntent(getIntent());
