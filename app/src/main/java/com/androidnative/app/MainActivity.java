7881dcd2d5417dd890e7be27229677e625e77963
Arbaz Malek <arbaz@otpless.com>
2024-04-04 14:35:14 +0530

    otpless headless sdk

--- app/src/main/java/com/androidnative/app/MainActivity.java
+++ app/src/main/java/com/androidnative/app/MainActivity.java
@@ -1,77 +1,166 @@
 package com.androidnative.app;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.otpless.dto.OtplessRequest;
-import com.otpless.dto.OtplessResponse;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.otpless.main.OtplessManager;
 import com.otpless.main.OtplessView;
+import com.otpless.dto.HeadlessRequest;
+import com.otpless.dto.HeadlessResponse;
+import com.otpless.dto.HeadlessChannelType;
 import com.otpless.utils.Utility;
 
-import org.json.JSONException;
+
 import org.json.JSONObject;
 
+
 public class MainActivity extends AppCompatActivity {
+
     OtplessView otplessView;
+
+    private EditText inputEditText, otpEditText;
+
+    private HeadlessChannelType channelType;
+    private TextView headlessResponseTv;
+
+    private Button otpverify,whatsappButton,gmailButton,twitterButton,slackButton,facebookButton,linkedinButton,microsoftButton;
+
+
+    final HeadlessRequest request = new HeadlessRequest("OQKCVGB7H42IZPVUMX7P"); //replace with your appid provided in documentation
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inputEditText = findViewById(R.id.input_text_layout);
+        otpEditText = findViewById(R.id.otp_et);
+        otpverify = findViewById(R.id.otpverify);
+        whatsappButton = findViewById(R.id.whatsapp_btn);
+        gmailButton = findViewById(R.id.gmail_btn);
+        twitterButton = findViewById(R.id.twitter_btn);
+        slackButton = findViewById(R.id.slack_btn);
+        facebookButton = findViewById(R.id.facebook_btn);
+        linkedinButton = findViewById(R.id.linkedin_btn);
+        microsoftButton = findViewById(R.id.microsoft_btn);
+        headlessResponseTv = findViewById(R.id.headless_response_tv);
 
-        //******************************************************** */
-        //This function will tell if WhatsApp is Installed or not.
-        // If you are using only whatsapp authentication then you can hide visibility of Login button using this function,
-        // if user doesn't have whatsapp installed on device.
-        //******************************************************** */
-
-        if (Utility.isWhatsAppInstalled(this)){
-            Toast.makeText(this, "whatsapp is installed in device", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this, "Whatsapp is not installed in device", Toast.LENGTH_SHORT).show();
-        }
-
-
-        // Initialise OtplessView
+        initTestingView();
+        // copy this code in onCreate of your Login Activity
         otplessView = OtplessManager.getInstance().getOtplessView(this);
-        OtplessRequest request = new OtplessRequest("RI7MXA25OHAGDHBXMFB1") //replace with your appid provided in documentation
-                                    .addExtras("crossButtonHidden","true");  //use this parameter to hide cross button
-        otplessView.setCallback(request, this::onOtplessCallback);
-        otplessView.showOtplessLoginPage(request, this::onOtplessCallback);
+        otplessView.setHeadlessCallback(getHeadlessRequest(), this::onHeadlessCallback);
+        otpverify.setOnClickListener(v -> {
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
         otplessView.verifyIntent(getIntent());
     }
 
-    private void onOtplessCallback(OtplessResponse response) {
-        if (response.getErrorMessage() != null) {
-    // todo error handing
-        } else {
-            final String token = response.getData().optString("token");
-    // todo token verification with api
-            Log.d("Otpless", "token: " + token);
-            Toast.makeText(this, "Token : " + token, Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(), SecondActivity.class);
-            i.putExtra("passing_token", token);
-            startActivity(i);
+    private HeadlessRequest getHeadlessRequest() {
+        final String input = inputEditText.getText().toString();
+
+        if (this.channelType != null) {
+            request.setChannelType(this.channelType);
+        }else {
+            if (!input.trim().isEmpty()) {
+                try {
+                    // parse phone number
+                    Long.parseLong(input);
+                    request.setPhoneNumber("+91", input);
+                } catch (Exception ex) {
+                    request.setEmail(input);
+                }
+
+            }
+            final String otp = otpEditText.getText().toString();
+            if (!otp.trim().isEmpty()){
+                request.setOtp(otp);
+            }
         }
+        return request;
+    }
+
+
+    private void initTestingView() {
+
+        // This code will be used to detect the WhatsApp installed status on the user's device.
+        if (Utility.isWhatsAppInstalled(this)) {
+            whatsappButton.setVisibility(View.VISIBLE);
+        } else {
+            whatsappButton.setVisibility(View.GONE);
+        }
+        //end
+
+        whatsappButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.WHATSAPP;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        gmailButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.GMAIL;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        twitterButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.TWITTER;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        slackButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.SLACK;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        facebookButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.FACEBOOK;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        linkedinButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.LINKEDIN;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
+
+        microsoftButton.setOnClickListener(view -> {
+            channelType = HeadlessChannelType.MICROSOFT;
+            otplessView.startHeadless(getHeadlessRequest(), this::onHeadlessCallback);
+        });
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        if (otplessView != null) {
-            otplessView.verifyIntent(intent);
-        }
+        otplessView.verifyIntent(intent);
     }
+
+    private void onHeadlessCallback(@NonNull final HeadlessResponse response) {
+        if (response.getStatusCode() == 200) {
+            JSONObject successResponse = response.getResponse();
+        } else {
+            String error = response.getResponse().optString("errorMessage");
+        }
+        headlessResponseTv.setText(response.toString());
+    }
+
+
     @Override
     public void onBackPressed() {
-        // make sure you call this code before super.onBackPressed();
         if (otplessView.onBackPressed()) return;
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
